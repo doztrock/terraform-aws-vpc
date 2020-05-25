c1cd60bccc70b12d0ad328144dd03728c234a707
Ivan Botero <ivan.botero@protonmail.ch>
2020-05-25 00:07:52 -0500

    Agregados comentarios y nuevas variables

--- variables.tf
+++ variables.tf
@@ -1,22 +1,52 @@
 variable "NAME" {
-  description = "VPC Name"
+  description = "Nombre de la VPC."
+}
+
+variable "DNS" {
+  description = "DNS asignados a la VPC."
+  default = [
+    "208.67.222.222",
+    "8.8.8.8",
+    "208.67.220.220",
+    "8.8.4.4"
+  ]
 }
 
 variable "CIDR" {
-  description = "VPC CIDR Block"
+  description = "Bloque CIDR asignado a la VPC"
 }
 
 variable "PUBLIC-SUBNET" {
-  description = "VPC Public Subnet"
+  description = "Listado de subredes publicas."
   default     = []
 }
 
 variable "PRIVATE-SUBNET" {
-  description = "VPC Private Subnet"
+  description = "Listado de subredes privadas."
   default     = []
 }
 
-variable "NAT" {
-  description = "VPC NAT"
-  default     = {}
+variable "NAT0" {
+  description = "VPC NAT Gateway #1"
+  default     = []
+}
+
+variable "NAT1" {
+  description = "VPC NAT Gateway #2"
+  default     = []
+}
+
+variable "NAT2" {
+  description = "VPC NAT Gateway #3"
+  default     = []
+}
+
+variable "NAT3" {
+  description = "VPC NAT Gateway #4"
+  default     = []
+}
+
+variable "NAT4" {
+  description = "VPC NAT Gateway #5"
+  default     = []
 }
